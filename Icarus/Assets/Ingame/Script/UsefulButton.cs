dbc8f258b6254bbac3707593c5a97997cced07dd
Seto-VG <131129286+Seto-VG@users.noreply.github.com>
2023-12-05 16:03:08 +0900

    [add] 実用的ボタンスクリプト
    これは作ってないです　ネットに転がってたやつです

---
+++ Icarus/Assets/Ingame/Script/UsefulButton.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class UsefulButton : SimpleButton
+{
+
+    [SerializeField]
+    RectTransform rectTransform;
+    [SerializeField]
+    float onPointerEnterScale = 1;
+    [SerializeField]
+    float onPointerDownScale = 1;
+    [SerializeField]
+    float duration = 0.1f;
+    [SerializeField]
+    Ease ease = Ease.OutQuad;
+
+    protected override void AdditionalOnPointerEnterProcess()
+    {
+        ChangeButtonScale(onPointerEnterScale);
+    }
+
+    protected override void AdditionalOnPointerExitProcess()
+    {
+        ChangeButtonScale(1);
+    }
+
+    protected override void AdditionalOnPointerDownProcess()
+    {
+        ChangeButtonScale(onPointerDownScale);
+    }
+
+    protected override void AdditionalOnPointerUpProcess()
+    {
+        ChangeButtonScale(1);
+    }
+
+    void ChangeButtonScale(float scale)
+    {
+        rectTransform.DOScale(Vector3.one * scale, duration).SetLink(gameObject).SetEase(ease).SetUpdate(true);
+    }
+
+}
