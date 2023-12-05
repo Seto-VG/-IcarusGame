d1e77bbbab2e3859b57cf16028f1a0d5d5584eaa
Seto-VG <131129286+Seto-VG@users.noreply.github.com>
2023-12-05 16:01:24 +0900

    [update] ゲームマネージャーオブジェクトの作成とノンシリアライズ化

--- Icarus/Assets/Ingame/Script/GameManager.cs
+++ Icarus/Assets/Ingame/Script/GameManager.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : SingletonBehavior<GameManager>
 {
+    [NonSerialized]
     public bool isComplete = false;
+    [NonSerialized]
     public bool isDeath = false;
     void Start()
     {
