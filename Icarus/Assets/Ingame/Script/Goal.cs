2bd27f6238333291675baf1d319a4ed223c26916
Seto-VG <131129286+Seto-VG@users.noreply.github.com>
2023-12-11 14:27:17 +0900

    [fix] goalスクリプトの移動とタグで判断するように変更

--- Icarus/Assets/Ingame/Script/Goal.cs
+++ Icarus/Assets/Ingame/Script/Goal.cs
@@ -4,21 +4,9 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerStay(Collider other)
     {
-        if(other.name == "Player")
+        if(other.CompareTag("player"))
         {
             GameManager.instance.OnCompleteStage();
         }
